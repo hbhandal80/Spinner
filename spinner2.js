16c66ef473cb7eddd07437b5d3789a82826be31c
Hervinder <hbhandal80@hotmail.com>
2021-05-05 00:47:30 +0000

    spinner2

---
+++ spinner2.js
@@ -0,0 +1,10 @@
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+let spinner = ['|', '/', '-', '\\', '|', '/', '-', '\\', '|', '\n']
+let delay = 100;
+
+for (const char of spinner) {
+  setTimeout(() => {
+    process.stdout.write(`\r ${char}`);
+  }, delay);
+  delay += 200;
+}
